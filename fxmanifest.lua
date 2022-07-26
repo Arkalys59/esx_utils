993e24c772031e511ee324734ce4f4b1086c9079
Mercury <43386730+XXXMercuryXXX@users.noreply.github.com>
2022-07-26 16:43:40 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'Mercury'
+
+description 'Snippet by Mercury'
+
+version '1.0.0'
+
+client_scripts {'main.lua'}
---
+++ main.lua
@@ -0,0 +1,41 @@
+local function SetWeaponDrops()
+    local handle, ped = FindFirstPed()
+    local finished = false
+
+    repeat
+        if not IsEntityDead(ped) then
+            SetPedDropsWeaponsWhenDead(ped, false)
+        end
+        finished, ped = FindNextPed(handle)
+    until not finished
+
+    EndFindPed(handle)
+end
+
+Citizen.CreateThread(function()
+	for i = 1, 12 do
+		Citizen.InvokeNative(0xDC0F817884CDD856, i, false)
+	end
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(5)
+        local myCoords = GetEntityCoords(GetPlayerPed(-1))
+        ClearAreaOfCops(myCoords.x, myCoords.y, myCoords.z, 1000.0, 0) --Enleve toute les voiture de pnj police
+
+        DisablePlayerVehicleRewards(PlayerId()) --Enleve les armes de toute les vehicule de police
+
+        SetWeaponDrops() --Enleve les armes des PNJ
+        
+    end
+end)
+
+--Train
+Citizen.CreateThread(function()
+    SwitchTrainTrack(0, true)
+    SwitchTrainTrack(3, true)
+    N_0x21973bbf8d17edfa(0, 120000)
+    SetRandomTrains(true)
+end)
+--Train
